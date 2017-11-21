1cc4948fc1fa47f11c8d3da6f0650f166d59b1f2
yyounis <youssef.younis@slu.edu>
2017-11-21 09:59:48 -0600

    Roxygenise Upload for Joint PDF Sample

--- R/Sample PDF.R
+++ R/Sample PDF.R
@@ -1,10 +1,12 @@
 #' @author Youssef Younis  <email: youssef.younis@slu.edu>
 #' @description This file aims to produce a sample from a given pdf
 #' @param PDF should be provided by user
-#' @param Lower Bounds and Upper Bounds should additional be provided by user
+#' @param Lower_Bounds and Upper_Bounds should additional be provided by user
 #' @param Optional: Specific interval may be inputed if available
 #' @import
 #' @return Specific Rejection Sample from provided PDF with given Lower/Upper Bounds
+#'
+#' @export
 
 sampleX <- function(f, lower_bound, upper_bound, interval = 0.01) {
  return(data.frame(x = seq(lower_bound, upper_bound, interval), y = f(x)))
