d952f022214e5ed2f467b025e45f0e3b7864df5e
yyounis <youssef.younis@slu.edu>
2017-11-23 12:37:57 -0600

    Roxygenise update and upload for Expected Value Sample

---
+++ R/Expected Value.R
@@ -0,0 +1,9 @@
+#' @author Youssef Younis  <email: youssef.younis@slu.edu>
+#' @description This file aims to produce an Expected Value of a g(x) or g(x,y) given a function g
+#' @param Function g should be provided for
+#' @param
+#' @param
+#' @import
+#' @return An Expected Value of a g(x) or g(x,y)
+#'
+#' @export
--- R/test1.R
+++ R/test1.R
@@ -1,9 +1,9 @@
 #' @author Youssef Younis  <email: youssef.younis@slu.edu>
 #' @description This file aims to produce a sample from a given Joint Density Function
 #' @param Joint Density Function should be provided by user
-#' @param Lower_Bounds and Upper_Bounds should additional be provided by user
-#' @param Optional: Specific interval may be inputed if available
+#' @param
+#' @param
 #' @import
-#' @return Specific Rejection Sample from provided PDF with given Lower/Upper Bounds
+#' @return Specific Rejection Sample from provided JDF
 #'
 #' @export
