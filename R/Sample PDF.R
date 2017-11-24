cbec2b8849bf6435e9786442960e2fe5c387417d
yyounis <youssef.younis@slu.edu>
2017-11-24 11:03:08 -0600

    Sample PDF coding add for boundaries of Rejection Sampling

--- R/Sample PDF.R
+++ R/Sample PDF.R
@@ -1,19 +1,29 @@
 #' @author Youssef Younis  <email: youssef.younis@slu.edu>
 #' @description This file aims to produce a sample from a given pdf
-#' @param PDF should be provided by user
-#' @param Lower_Bounds and Upper_Bounds should additional be provided by user
-#' @param Optional: Specific interval may be inputed if available
+#' @param f should be provided by user
+#' @param lower_bound provided by user for closed specific sample
+#' @param upper_bound provided by user for closed specific sample
+#' @param interval Default value provided but can be altered if necessary
 #' @import
 #' @return Specific Rejection Sample from provided PDF with given Lower/Upper Bounds
 #'
 #' @export
 
 sampleX <- function(f, lower_bound, upper_bound, interval = 0.01) {
- return(data.frame(x = seq(lower_bound, upper_bound, interval), y = f(x)))
+Sample1 <- mutate(data.frame(x = seq(lower_bound, upper_bound, interval), y = f(x)))
+return(Sample1)
 }
 
+X <- seq(lower_bound, upper_bound, .001)
+Y <- gx(x)
+C <- max(Y) + 10
+
+
 gx <- function(x){return(4*x)}
-sampleX(f = gx(x), lower_bound = 0, upper_bound = sqrt(.5))
+SpecificSample <- mutate(data.frame(x = seq(0, sqrt(.5), .01)), y = gx(x))
+
+library (dplyr)
+install.packages("dplyr")
 
 
 
