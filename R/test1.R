95bf3b9e201a3a3163a07a044c3c1e4b063a3bda
yyounis <youssef.younis@slu.edu>
2017-11-28 10:37:20 -0600

    Roxygenise Edit

--- R/test1.R
+++ R/test1.R
@@ -1,3 +1,4 @@
+#' @title Test
 #' @author Youssef Younis  <email: youssef.younis@slu.edu>
 #' @description This file aims to produce a sample from a given Joint Density Function
 #' @param Joint Density Function should be provided by user
@@ -7,26 +8,16 @@
 #' @param upper_boundx provided by user to calculate maximum boundary of "X"
 #' @param upper_boundy provided by user to calculate maximum boundary of "Y"
 #' @param upper_boundJDF provided by user to calculate maximum boundary of "JDF"
-#' @example \dontrun{gx = function(x,y) {
-return (x+y)
-}}
-SampleJDF(0, 0, 1, 1, 0, 2, gx)
+#' @examples gx = function(x,y){
+#' return (x+y)}
+#' SampleJDF(0, 0, 1, 1, 0, 2, gx)
 #' @import dplyr
 #' @return Specific Rejection Sample from provided JDF
 #'
 #' @export
-
-
 SampleJDF <- function(lower_boundx, lower_boundy, upper_boundx, upper_boundy, lower_boundJDF, upper_boundJDF, f = f, Rep = 10000){
-  
+
 SamData <- replicate(Rep, {pSx <- runif(1, lower_boundx, upper_boundx); pSy <- runif(1, lower_boundy, upper_boundy); if(runif(1,lower_boundJDF, upper_boundJDF) < f(x = pSx, y = pSy)) {c(x = pSx, y = pSy)} else c(x = NA,y = NA)})
 SamData <- data.frame(t(SamData))
 return(SamData)
 }
-
-#Example
-
-gx = function(x,y) {
-return (x+y)
-}
-SampleJDF(0, 0, 1, 1, 0, 2, gx)
