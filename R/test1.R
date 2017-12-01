45707921edb61982a8f4ebb0a29e12406449ab8d
yyounis <youssef.younis@slu.edu>
2017-12-01 13:45:51 -0600

    Title Change

--- R/test1.R
+++ R/test1.R
@@ -1,4 +1,4 @@
-#' @title Test
+#' @title SampleJDF
 #' @author Youssef Younis  <email: youssef.younis@slu.edu>
 #' @description This file aims to produce a sample from a given Joint Density Function
 #' @param Joint Density Function should be provided by user
