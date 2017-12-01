577a82945449bbc957a9e4fd9569f168343329ea
yyounis <youssef.younis@slu.edu>
2017-12-01 14:23:48 -0600

    Additional paramater added to Probabilities

---
+++ R/Probability.R
@@ -0,0 +1,18 @@
+#' @title Probability Related to X or X and Y
+#' @author Youssef Younis  <email: youssef.younis@slu.edu>
+#' @description This file aims to produce a Probability of an X or an X and Y
+#' @param f the PDF provided by the user
+#' @param
+#' @param variable provided by user to suggest which variable they want to use
+#' @param upper_bound1 The maximum value of the probability distribution
+#' @param upper_bound2 The upperbound assigned by the user to calculate the probabilities
+#' @param lower_bound1 The minimum value of the probability distribution
+#' @param lower_bound2 The lowerbound assigned by the suer to calculate the probabilities
+#' @import dplyr
+#' @return A probability related to X or X and Y
+#'
+#' @export
+
+Prob <- function(variable, upper_bound1, upper_bound2, lower_bound1, lower_bound2) {
+
+}
