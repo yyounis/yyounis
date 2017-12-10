e9f8a57267936307e34ecf5a29e2941a8efe4f9c
yyounis <youssef.younis@slu.edu>
2017-12-10 10:35:42 -0600

    Suppress Messages in Vignette

--- vignettes/rejection_sampling.R
+++ vignettes/rejection_sampling.R
@@ -16,3 +16,15 @@ gx = function(x,y){
 return (x+y)}
 head(SampleJDF(0, 0, 1, 1, 0, 2, gx)) %>% rename("Random Sample X-Coordinate" = "x", "Random Sample Y-Coordinate" = "y")
 
+## ------------------------------------------------------------------------
+f = function(x) {return(2*x)}
+ExpectedValue(f, "X", lower_boundx = 0, upper_boundx = 1)
+
+## ------------------------------------------------------------------------
+f = function(x) {return(1/2/pi *(sin(x) + 1))}
+ExpectedValue(f, "X", lower_boundx = 0, upper_boundx = 2* pi)
+
+## ------------------------------------------------------------------------
+f = function(x,y) {return(x+y)}
+ExpectedValue(f, variable = "XY", upper_boundx = 1, upper_boundy = 1, upper_boundF = 2, lower_boundx = 0, lower_boundy = 0, lower_boundF = 0)
+
