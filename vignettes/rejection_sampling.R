d57dd4321e3a24a8b59f66565c20e6a690248ad9
yyounis <youssef.younis@slu.edu>
2017-12-10 08:36:57 -0600

    Vignette update

--- vignettes/rejection_sampling.R
+++ vignettes/rejection_sampling.R
@@ -1,10 +1,15 @@
 ## ------------------------------------------------------------------------
 set.seed(123)
+library(dplyr)
 gx <- function(x){return(4*x)}
-SpecificSample <- dplyr::mutate(data.frame(x = seq(0, sqrt(.5), .01)), y = gx(x))
+SpecificSample <- mutate(data.frame( x = seq(0, sqrt(.5), .01)), y = gx(x))
 max_c(0, sqrt(.5), y = 4*x)
+
+## ------------------------------------------------------------------------
 ApproveReject(0, sqrt(.5), y = gx(x))
-head(Replication(0, sqrt(.5), y = gx(x)))
+
+## ------------------------------------------------------------------------
+head(Replication(0, sqrt(.5), y = gx(x))) %>% rename("Random Sample" = "x")
 
 ## ---- fig.show='hold'----------------------------------------------------
 plot(1:10)
