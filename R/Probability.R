bbc9476e156accd2e92c78b502d31f930c0614ca
yyounis <youssef.younis@slu.edu>
2017-12-10 08:33:02 -0600

    Finalized Probability File

--- R/Probability.R
+++ R/Probability.R
@@ -31,32 +31,35 @@
 #' @export
 
 Prob <- function(f, variable, upper_boundx, upper_boundy, upper_boundF, upper_boundP, lower_boundx, lower_boundy, lower_boundF, lower_boundP, Rep = 10000) {
-if (variable == "X") {
-  #User is ONLY looking at the probability of X
-  max_c <- function(lower_bound, upper_bound, y = f)
-  {X <- seq(lower_bound, upper_bound, .001)
-  Y <- f(X)
-  C <- max(Y) + 10
-  return(C)}
-  PotentialSample <- runif(1, lower_boundx, upper_boundx)
-  Result <- ifelse(runif(1, lower_boundx, max_c(lower_boundx, upper_boundx, y = f)) < f(PotentialSample), PotentialSample, NA)
-  BigSample <- data.frame(x = replicate(Rep, {PotentialSample <- runif(1, lower_boundx, upper_boundx);
-  ifelse(runif(1, lower_boundx, max_c(lower_boundx, upper_boundx, y = f)) < f(PotentialSample), PotentialSample, NA)}))
-  if(exists("upper_boundP")){return(mean(BigSample$x < upper_boundP, na.rm = TRUE))}else{return(mean(BigSample$x > lower_boundP, na.rm = TRUE))}
-
-} else {
-  #User is looking at BOTH X and Y probability combined
-  SamData <- replicate(Rep, {pSx <- runif(1, lower_boundx, upper_boundx); pSy <- runif(1, lower_boundy, upper_boundy); if(runif(1,lower_boundF, upper_boundF) < f(x = pSx, y = pSy)) {c(x = pSx, y = pSy)} else c(x = NA,y = NA)})
-  SamData <- data.frame(t(SamData))
-  SamData <- SamData[complete.cases(SamData),]
-  if (exists("upper_boundP") & exists("lower_boundP")){
-    mean((SamData$x + SamData$y < upper_boundP & SamData$x + SamData$y > lower_boundP), na.rm = TRUE)
+  if (variable == "X") {
+    #User is ONLY looking at the probability of X
+    max_c <- function(lower_bound, upper_bound, y = f){
+      X <- seq(lower_bound, upper_bound, .001)
+      Y <- f(X)
+      C <- max(Y) + 10
+      return(C)}
+    PotentialSample <- runif(1, lower_boundx, upper_boundx)
+    Result <- ifelse(runif(1, lower_boundx, max_c(lower_boundx, upper_boundx, y = f)) < f(PotentialSample), PotentialSample, NA)
+    BigSample <- data.frame(x = replicate(Rep, {PotentialSample <- runif(1, lower_boundx, upper_boundx);
+    ifelse(runif(1, lower_boundx, max_c(lower_boundx, upper_boundx, y = f)) < f(PotentialSample), PotentialSample, NA)}))
+    if(hasArg(upper_boundP)){
+      return(mean(BigSample$x < upper_boundP, na.rm = TRUE))
+    }else{
+      return(mean(BigSample$x > lower_boundP, na.rm = TRUE))}
+  } else {
+    #User is looking at BOTH X and Y probability combined
+    SamData <- replicate(Rep, {pSx <- runif(1, lower_boundx, upper_boundx); pSy <- runif(1, lower_boundy, upper_boundy); if(runif(1,lower_boundF, upper_boundF) < f(x = pSx, y = pSy)) {c(x = pSx, y = pSy)} else c(x = NA,y = NA)})
+    SamData <- data.frame(t(SamData))
+    SamData <- SamData[complete.cases(SamData),]
+    if (hasArg(upper_boundP) & hasArg(lower_boundP)){
+      mean((SamData$x + SamData$y < upper_boundP & SamData$x + SamData$y > lower_boundP), na.rm = TRUE)
+    }
+    else if (hasArg(upper_boundP) & !hasArg(lower_boundP)){
+      mean(SamData$x + SamData$y < upper_boundP, na.rm = TRUE)
+    }
+    else if(hasArg(lower_boundP) & !hasArg(upper_boundP)){
+      mean(SamData$x + SamData$y > lower_boundP, na.rm = TRUE)}
   }
-  else if (exists("upper_boundP") & !exists("lower_boundP")){
-    mean(SamData$x + SamData$y < upper_boundP, na.rm = TRUE)
-  }
-  else (exists("lower_boundP") & !exists("upper_boundP"))
-  mean(SamData$x + SamData$y > lower_boundP, na.rm = TRUE)}
 }
 
 
@@ -65,9 +68,9 @@ f = function(x) {return(4*x)}
 Prob(f, variable = "X", upper_boundx = .5, upper_boundP = .3, lower_boundx = 0, lower_boundP = 0, Rep = 10000)
 
 #ExampleSpeegle
-##f = function(x) {return(1/2/pi *(sin(x) + 1))}
-##Prob(f, variable = "X", upper_boundx = 2*pi, lower_boundP = pi, lower_boundx = 0, Rep = 10000)
+f = function(x) {return(1/2/pi *(sin(x) + 1))}
+Prob(f, variable = "X", upper_boundx = 2*pi, lower_boundP = pi, lower_boundx = 0, Rep = 10000)
 
 #Example3
-##f = function(x,y) {return(x+y)}
-##Prob(f, variable = "XY", upper_boundx = 1, upper_boundy = 1, upper_boundF = 2, upper_boundP = 1, lower_boundx = 0, lower_boundy = 0, lower_boundP = 0, lower_boundF = 0, Rep = 10000)
+f = function(x,y) {return(x+y)}
+Prob(f, variable = "XY", upper_boundx = 1, upper_boundy = 1, upper_boundF = 2, upper_boundP = 1, lower_boundx = 0, lower_boundy = 0, lower_boundP = 0, lower_boundF = 0, Rep = 10000)
